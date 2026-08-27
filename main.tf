6f4ac44904a12b54798ae38f92019a03d7e5fd0b
Gaurav kumar <gauravk.evolve@gmail.com>
2026-08-28 00:45:01 +0530

    Add AMI lookup and EC2 key resources

---
+++ main.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+resource "tls_private_key" "ec2_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "ec2_key" {
+  key_name   = "custom-vpc-ec2-key"
+  public_key = tls_private_key.ec2_key.public_key_openssh
+}
